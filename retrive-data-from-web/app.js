23fe89a314fa5a6714871b5dd3d879763abb3da9
lilylin395135@gmail.com <lilylin395135@gmail.com>
2024-04-08 18:21:47 +0800

    error handling

--- retrive-data-from-web/app.js
+++ retrive-data-from-web/app.js
@@ -6,23 +6,32 @@ function printMessage(username, badgeCount, points) {
 }
 
 function getProfile(username) {
-    //connect to the API URL (https://teamtreehouse.com/profiles/lilylin2.json)
-    const request = https.get(
-        `https://teamtreehouse.com/profiles/${username}.json`,
-        (response) => {
-            let body = "";
-            // console.dir(response.statusCode); //和console.log();差不多
-            //Read the data
-            response.on('data', (data) => {
-                body += data.toString();
+    try {
+        //connect to the API URL (https://teamtreehouse.com/profiles/lilylin2.json)
+        const request = https.get(
+            `https://teamtreehouse.com/profiles/${username}.json`,
+            (response) => {
+                let body = "";
+                // console.dir(response.statusCode); //和console.log();差不多
+                //Read the data
+                response.on('data', (data) => {
+                    body += data.toString();
+                });
+                response.on('end', () => {
+                    //Parse the data
+                    let profile = JSON.parse(body);//把回覆的JSON格式轉成JS物件，more readable
+                    // console.dir(profile.points);//profile.points是個物件
+                    printMessage(username, profile.badges.length, profile.points.JavaScript);
+                });
             });
-            response.on('end', () => {
-                //Parse the data
-                let profile = JSON.parse(body);//把回覆的JSON格式轉成JS物件，more readable
-                // console.dir(profile.points);//profile.points是個物件
-                printMessage(username, profile.badges.length, profile.points.JavaScript);
-            });
+        //Error handling
+        request.on('error', (error) => {
+            console.error(`Problem with request: ${error.message}`);
         });
+    }
+    catch (error) {
+        console.error(error.message);
+    }
 }
 const users = process.argv.slice(2);//取得命令列參數，去掉前面兩個元素
 users.forEach(getProfile);//JS寫法把users陣列裡的每個元素都傳進getProfile函式裡
