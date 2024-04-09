5d256c3e7c2defc1dfabb7df06b3c79a8eaca008
lilylin395135@gmail.com <lilylin395135@gmail.com>
2024-04-09 10:22:07 +0800

    取得Router header的參數

--- express/01_初體驗.js
+++ express/01_初體驗.js
@@ -5,32 +5,25 @@ const express = require('express');
 const app = express();
 
 //建立一個路由
-app.get('/home', (req, res) => {
+app.get('/request', (req, res) => {
+    //原生操作(使用原本的語言、基礎，不用打包好的框架，這裡就是不用Express)
+    console.log(req.method);//請求的方法 //回傳了GET
+    console.log(req.url);//請求的路徑 //用網址http://localhost:3000/Request?a=100&b=200 回傳了"/Request?a=100&b=200"
+    console.log(req.httpVersion);//請求的HTTP版本 //回傳了1.1
+    console.log(req.headers);//請求的標頭 //回傳一串JSON
+
+    //Express操作
+    console.log(req.path);//請求的路徑 //用網址http://localhost:3000/Request?a=100&b=200 回傳了"/Request"
+    console.log(req.query);//請求的查詢參數 //用網址http://localhost:3000/Request?a=100&b=200 回傳了{ a: '100', b: '200' }
+    //取得使用者IP
+    console.log(req.ip);//回傳::1
+    //取得請求header
+    console.log(req.get('host'));//回傳localhost:3000
+
+
+
     res.end('Hello World!');
 });//瀏覽器把請求發送過來後，如果是get請求，且路徑是'/'，則執行這個函數，並且返回'Hello World!'
-
-//每個網站的首頁幾乎都是用'/'表示，所以這裡用'/'表示首頁
-app.get('/', (req, res) => {
-    res.end('Home');
-});
-
-//Post
-app.post('/login', (req, res) => {
-    res.end('login login');
-});
-
-//可以使用all方法，任何請求方法都可以，只要路徑是對的，就會執行這個函數
-//1.Get方法→直接網址http://127.0.0.1:3000/test，前往成功
-//2.Post方法→html按鈕網址改http://127.0.0.1:3000/test，前往成功
-app.all('/test', (req, res) => {
-    res.end('test test');
-});
-
-//404 Not Found
-//如果上面的路由都沒有匹配成功，則執行這個函數
-app.all('/*', (req, res) => {
-    res.end('404 Not Found!');
-});
 
 //設定伺服器監聽的埠號
 app.listen(3000, () => {
