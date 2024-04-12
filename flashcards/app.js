030feb13ecca3a3d69a054edb774dfa052babde6
lilylin395135@gmail.com <lilylin395135@gmail.com>
2024-04-12 20:58:17 +0800

    card.pug let its variable can be rendered.

--- flashcards/app.js
+++ flashcards/app.js
@@ -1,24 +1,31 @@
-
 //require方法使用套件Express，放進express變數中，之後都用此變數操作express
 const express = require("express");
 
 //啟用一個Express應用程式
 const app = express();
 
-app.set("view engine","pug");//set方法做Express的設置，設定view engine為pug
+app.set("view engine", "pug"); //set方法做Express的設置，設定view engine為pug
 
 //建立一個路由
-app.get("/",(request,response)=>{
-    response.render("index"); //回應一個index.pug檔案
+app.get("/", (request, response) => {
+  response.render("index"); //回應一個index.pug檔案
 });
 
 //建立第二個路由，第二個頁面
-app.get("/about",(request,response)=>{
-    //回應一個字串
-    response.send("<h1>JavaScript & Express, Hi.</h1>");
+app.get("/cards", (request, response) => {
+  //傳入prompt變數到card.pug檔案，方法一
+  response.locals.prompt = "Who is buried in Grant's tomb?"; //locals是Express的一個物件，可以存放一些變數，這裡存放prompt變數
+  response.render("card");
+  //傳入prompt變數到card.pug檔案，方法二
+  response.render("card", {
+    prompt: "Who is buried in Grant's tomb?",
+    hint: "Think about whose tomb it is.",
+  });
 });
 
 //建立伺服器路由port 3000
-app.listen(3000,()=>{
-    console.log("Server is running on http://localhost:3000 or http://127.0.0.1:3000/");
+app.listen(3000, () => {
+  console.log(
+    "Server is running on http://localhost:3000 or http://127.0.0.1:3000/"
+  );
 });
