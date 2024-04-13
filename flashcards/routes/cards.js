f2b796b84105bacf0f484169caed0f09822739ea
lilylin395135@gmail.com <lilylin395135@gmail.com>
2024-04-13 13:27:20 +0800

    feat: use redirect to show random url of cards/ feat:only key in id no side also redirect

--- flashcards/routes/cards.js
+++ flashcards/routes/cards.js
@@ -6,6 +6,13 @@ const router = express.Router();
 const { data } = require("../data/flashcardData.json"); //引入flashcardData.json檔案
 const { cards } = data; //separated out the cards 解構賦值，取得data物件的cards屬性
 
+//show random card
+router.get("/", (request, response) => {
+    const numberOfCards = cards.length; //取得cards陣列的長度
+    const flashcardId = Math.floor(Math.random() * numberOfCards); //取得一個隨機數字
+    response.redirect(`/cards/${flashcardId}?side=question`); //重新導向到/cards/flashcardId?side=question
+  });
+
 //因為我們在app.js中設定了app.use("/cards",cardRoutes);
 //所以這裡的路由都是/cards開頭，可以只寫"/"，不用寫"/cards"
 //用冒號( colon ) 告訴express冒號後有參數id (a route parameter called id)
@@ -13,6 +20,9 @@ router.get("/:id", (request, response) => {
     //query string to get question or answer
     const { side } = request.query; //解構賦值，取得query物件的side屬性
     const { id } = request.params; //解構賦值，取得params物件的id屬性
+    if (!side) { //如果side不存在
+      response.redirect(`/cards/${id}?side=question`); //重新導向到/cards/id?side=question
+    }
     const text = cards[id][side]; //取得cards陣列的id索引的side屬性
     const { hint } = cards[id]; //解構賦值，取得cards陣列的id索引的hint屬性
 
