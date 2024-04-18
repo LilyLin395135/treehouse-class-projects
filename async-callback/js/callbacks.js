ac80a8201fb7cdc0c0195cf73697efab9a1718c8
lilylin395135@gmail.com <lilylin395135@gmail.com>
2024-04-18 19:55:26 +0800

    feat:add a callback after get astrosUrl's data, then get data from wiki by every astroname, finish Wiki then generateHtml

--- async-callback/js/callbacks.js
+++ async-callback/js/callbacks.js
@@ -4,13 +4,15 @@ const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
 // xhr常被用來Make an AJAX request
-function getJSON(url) {
+//加上callback function，從astrosUrl拿回太空人員的資料後，再執行callback去拿wiki的資料
+//getJSON用url參數做AJAX request，並且在請求完成時執行callback function
+function getJSON(url, callback) {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', url);
   xhr.onload = () => {
     if(xhr.status === 200) {
-      let data = JSON.parse(xhr.responseText);
-      console.log(data);
+      let data = JSON.parse(xhr.responseText);//astrosUrl拿回的資料
+      return callback(data);//執行callback function,去拿wiki的資料
     }
   };//當請求完成時，會執行這個函數這是一個callback function
   xhr.send();//發送請求
@@ -41,4 +43,19 @@ function generateHTML(data) {
 //add event listener, click the button to getJSON
 //callback function occurs only when the button is clicked
 //a click will add a task to the callback queue, each event will eventually go onto the call stack to be executed
-btn.addEventListener('click', () => getJSON(astrosUrl));
+btn.addEventListener('click', () => {
+  getJSON(
+    astrosUrl,
+    (json) => {//astrosUrl完成後的資料，我們放進json變數
+      //json結構是物件，裡面有一個key屬性people，裡面是一個陣列，裡面是太空人員的資料有name和craft
+      json.people.map( person => {//person代表people中每一個人
+        getJSON(
+          //放入wikiUrl+person.name，去拿wiki的資料
+          wikiUrl + person.name,//完成後就會執行generateHTML，建立append new DOM elements，顯示太空人在網頁上
+          generateHTML
+        );
+      });
+
+    }
+
+  );});
